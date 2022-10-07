b22e0c871372e5df4590f29aaea215516cbd692d
Myzynets <anvostok25@gmail.com>
2022-10-07 20:27:33 +0300

    configs

--- static/sendData.js
+++ static/sendData.js
@@ -3,7 +3,7 @@ function SendJSON() {
     let name = document.querySelector('#username');
     let mail = document.querySelector('#user-email');
     let pass = document.querySelector('#user-pass');
-    let url = conf.urlConf;
+    let url = window.location.origin+"/post";
     let data=JSON.stringify({"name":name.value, "mail":mail.value, "pass":pass.value});
 
     fetch(url,{
